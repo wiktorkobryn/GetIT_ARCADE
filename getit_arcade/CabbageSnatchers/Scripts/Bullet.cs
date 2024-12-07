f9a780a2e138d67ce3da88bfb47a71d6e133d98e
wiktorkobryn <wiktor.kobryn@wp.pl>
2024-12-07 08:17:23 +0100

    fixes & balance

--- getit_arcade/CabbageSnatchers/Scripts/Bullet.cs
+++ getit_arcade/CabbageSnatchers/Scripts/Bullet.cs
@@ -3,23 +3,23 @@ using System;
 
 public partial class Bullet : Node2D
 {
-  public const float SPEED = 2500;
+  [Export]
+  public float SPEED = 2500;
   public Vector2 direction = Vector2.Zero;
   public float rotation = 0;
 
   public override void _Process(double delta)
   {
-    Position += direction * SPEED * (float)delta;
-    Rotation = rotation;
+	Position += direction * SPEED * (float)delta;
+	Rotation = rotation;
   }
   private void OnDestroyTimerTimeout()
   {
-    QueueFree();
+	QueueFree();
   }
 
   private void OnHitboxAreaEntered(Area2D area)
   {
-    QueueFree();
+	QueueFree();
   }
 }
-
